4b9403691e373f060a78ddbc9c7b88f80ac448cc
Hans Fischer <mail@hans-fischer.com>
2025-09-13 22:15:54 +0200

    fix: remove old tests

--- internal/proxy/handlers/bucket/handlers_test.go
+++ internal/proxy/handlers/bucket/handlers_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -315,4 +316,27 @@ func TestBucketSubResourceHandlerMethods(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestMainBucketHandler_NewHandlers(t *testing.T) {
+	// Test that the main bucket handler correctly initializes all new sub-handlers
+	mockS3Client := &MockS3Client{}
+	logger := logrus.NewEntry(logrus.New())
+
+	handler := NewHandler(mockS3Client, logger, "test-prefix")
+
+	// Verify all handlers are initialized
+	assert.NotNil(t, handler.GetVersioningHandler())
+	assert.NotNil(t, handler.GetTaggingHandler())
+	assert.NotNil(t, handler.GetNotificationHandler())
+	assert.NotNil(t, handler.GetLifecycleHandler())
+	assert.NotNil(t, handler.GetReplicationHandler())
+	assert.NotNil(t, handler.GetWebsiteHandler())
+	assert.NotNil(t, handler.GetAccelerateHandler())
+	assert.NotNil(t, handler.GetRequestPaymentHandler())
+	assert.NotNil(t, handler.GetACLHandler())
+	assert.NotNil(t, handler.GetCORSHandler())
+	assert.NotNil(t, handler.GetPolicyHandler())
+	assert.NotNil(t, handler.GetLocationHandler())
+	assert.NotNil(t, handler.GetLoggingHandler())
 }
